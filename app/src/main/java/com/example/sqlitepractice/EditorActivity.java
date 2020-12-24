e4ec8683da20c50af285777bc14dc54fe465e39d
Rheeshabh kumar singh <rishabhs861@gmail.com>
2020-12-24 23:36:41 +0530

    -added floating action button -implemented custom toolbar -added editor layout

--- app/src/main/java/com/example/sqlitepractice/EditorActivity.java
+++ app/src/main/java/com/example/sqlitepractice/EditorActivity.java
@@ -1,4 +1,32 @@
 package com.example.sqlitepractice;
 
-public class EditorActivity {
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+import com.google.android.material.appbar.MaterialToolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EditorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+
+        MaterialToolbar toolbar =(MaterialToolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_items, menu);
+
+
+        return true;
+
+    }
 }
--- app/src/main/java/com/example/sqlitepractice/MainActivity.java
+++ app/src/main/java/com/example/sqlitepractice/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.sqlitepractice;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +20,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        MaterialToolbar toolbar =(MaterialToolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
         fab = (FloatingActionButton) findViewById(R.id.floatingActionButton);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFAB();
+            }
+        });
+    }
+
+    public void openFAB()
+    {
+        Intent intent;
+        intent = new Intent(this, EditorActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.first_menu, menu);
+        return true;
     }
 }
